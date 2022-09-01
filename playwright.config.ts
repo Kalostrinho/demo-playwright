bdd5a604dbda81bdab0dae84fb151d9d1948ee78
AlanZinho <jfkalostro@gmail.com>
2022-09-01 16:32:38 -0500

    chore(config): Setup framework configuration and folder structure

---
+++ playwright.config.ts
@@ -0,0 +1,54 @@
+import type { PlaywrightTestConfig } from '@playwright/test'
+import { devices } from '@playwright/test'
+
+const baseDir: string = './pw-results'
+const reportsDir: string = `${baseDir}/reports`
+const config: PlaywrightTestConfig = {
+  testDir: './tests',
+  outputDir: baseDir,
+  testMatch: /.*\.spec\.ts/,
+  timeout: 180 * 1000,  // << 3 minutes
+  expect: {
+    timeout: 10000
+  },
+  fullyParallel: true,
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,  // << To identify flakiness
+  workers: process.env.CI ? 10 : undefined,
+  reporter: [
+    ['junit', { outputFile: `${reportsDir}/pw-junit-report.xml` }],
+    ['json', { outputFile: `${reportsDir}/pw-json-report.json` }],
+    ['html', { outputFolder: `${reportsDir}/html-report`, open: 'never' }],
+    ['list']
+  ],
+  projects: [
+    {
+      name: 'chromium',
+      use: {
+        ...devices['Desktop Chrome'],
+      },
+    },
+    {
+      name: 'firefox',
+      use: {
+        ...devices['Desktop Firefox'],
+      },
+    },
+    {
+      name: 'webkit',
+      use: {
+        ...devices['Desktop Safari'],
+      },
+    },
+  ],
+  use: {
+    // baseURL: 'http://localhost:3000',
+    navigationTimeout: 30000,
+    actionTimeout: 10000,
+    ignoreHTTPSErrors: true,
+    trace: 'on',
+    video: 'on',
+    screenshot: 'on',
+  },
+}
+export default config
